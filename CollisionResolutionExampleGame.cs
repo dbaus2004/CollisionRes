c2c0da6460c3e9ef65395e164e3bfdaee278648e
dbaus2004 <dbaus2004@gmail.com>
2026-09-17 19:15:02 -0500

    meow

--- CollisionResolutionExampleGame.cs
+++ CollisionResolutionExampleGame.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 
 namespace CollisionResolutionExercise;
@@ -11,7 +12,7 @@ namespace CollisionResolutionExercise;
 public class CollisionResolutionExampleGame : Game
 {
     // private variables
-    private GraphicsDeviceManager graphics;
+    private GraphicsDeviceManager graphics; 
     private SpriteBatch spriteBatch;
     private List<BallSprite> balls;
 
@@ -82,7 +83,30 @@ public class CollisionResolutionExampleGame : Game
                     balls[j].Colliding = true;
 
                     // TODO: Handle collisions
+                    balls[i].Center -= balls[i].Velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    balls[j].Center -= balls[j].Velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
+                    Vector2 collisionAxis = balls[i].Center - balls[j].Center;
+                    collisionAxis.Normalize();
+                    float angle = (float)System.Math.Acos(Vector2.Dot(collisionAxis, Vector2.UnitX));
+
+                    float m0 = balls[i].Mass;
+                    float m1 = balls[j].Mass;
+                    
+                    Vector2 u0 = Vector2.Transform(balls[i].Velocity, Matrix.CreateRotationZ(-angle));
+                    Vector2 u1 = Vector2.Transform(balls[j].Velocity, Matrix.CreateRotationZ(-angle));
+
+                    Vector2 v0;
+                    Vector2 v1;
+
+                    v0.X = ((m0 - m1) / (m0 + m1)) * u0.X + ((2 * m1) / (m0 + m1)) * u1.X;
+                    v1.X = ((2 * m0) / (m0 + m1)) * u0.X + ((m1 - m0) / (m0 + m1)) * u1.X;
+                    v0.Y = u0.Y;
+                    v1.Y = u1.Y;
+
+                    balls[i].Velocity = Vector2.Transform(v0, Matrix.CreateRotationZ(angle));
+                    balls[j].Velocity = Vector2.Transform(v1, Matrix.CreateRotationZ(angle));
+                    
                 }
             }
         } 
